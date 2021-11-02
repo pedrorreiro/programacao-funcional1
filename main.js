74b0169b90358d7721f4cb48d19327b603cc330f
Pedro <pedro_rique17@hotmail.com>
2021-11-02 17:26:40 -0300

    exercicio 4

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ console.log(addedApps.join('\n'));
     */
 
 // (Exercício 4) Identifique quais funções são high-order
-    // Adicione um comentário identificando-as
+    /* R.curry(), pois ela recebe uma função como argumento */
 
 // (Exercício 5) Crie um novo arquivo main2.js (baseado no main.js) que seleciona todos as apps adicionadas
 // depois do ano 2000 e atualizadas em 2020. Ao imprimir as apps, além do nome, imprima
